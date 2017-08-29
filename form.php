f3757ec40a8dda722ef98cb721e866f6cb38e946
JAM2 <jmistri7@gmail.com>
2017-08-28 22:33:48 -0700

    Adding file ownership fix

--- form.php
+++ form.php
@@ -57,6 +57,7 @@ if (isset($_POST['first_name'])) {
 			exec('echo "  CustomLog ${APACHE_LOG_DIR}/access.log combined" >> /etc/apache2/sites-available/' . $linux_username . '.lahs.club.conf');
 			exec('echo "</VirtualHost>" >> /etc/apache2/sites-available/' . $linux_username . '.lahs.club.conf');
 			exec('cp /root/index.php /var/www/' . $linux_username . '/');
+			exec('chown ' . $linux_username . ':' . $linux_username . ' /var/www/' . $linux_username . '/index.php');
 			exec('a2ensite ' . $linux_username . '.lahs.club.conf');
 			exec('service apache2 restart');
 
