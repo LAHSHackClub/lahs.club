19a2f3bb6dec058a9d4d8ed8972c2ff966e93d52
SDBagel <sdbagel@gmail.com>
2021-11-06 21:15:13 -0700

    Remove other PHP files for security

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require_once("conf.ini.php");
-
-if (isset($_SESSION['logged_in']) && isset($_POST['email'])) {
-	$connection = mysqli_connect("127.0.0.1", $username, $password, $database);
-	mysqli_query($connection, "DELETE FROM `users` WHERE `email` = '" . $_POST['email'] . "';");
-	echo "success";
-}
